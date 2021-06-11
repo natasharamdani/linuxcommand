2da0cb0de9a0cc9be1f85eeb8913b57e9d50fba7
mardefasma <marde.aza@tokopedia.com>
2021-06-11 17:22:08 +0700

    update screenshot

--- script.sh
+++ script.sh
@@ -1,1 +1,4 @@
-awk -F',' '{print $1, $2, $4 - $3|"sort -nrk3"}' workshop.csv | sed -n '1p' | awk '{print $1, $2, int($3/31556926)}'
+awk -F',' '{print $1, $2, $4 - $3|"sort -nrk3"}' workshop.csv | sed -n '1p' | awk '{print $1","$2","int($3/31556926)}'
+
+# marde.aza@BA-00016843 linuxcommand % bash script.sh
+# Eugenia,Moya,46
