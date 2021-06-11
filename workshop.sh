f72cab75b4dbe0f5564f4824052289376094591f
Setya Wibawa <setyawibawa58@gmail.com>
2021-06-11 20:00:17 +0700

    update done

--- workshop.sh
+++ workshop.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-a=`date +%s`
-cat workshop.csv | sort -t, -nk4 -r | awk -F "," 'NR == 1 {print $1, $2, $4-$3}'}
+cat workshop.csv | awk -F "," '{print $1,$2,$4-$3}' | sort -t" " -nk3 -r | awk -F " " 'NR == 1 {print $1","$2","int($3/(365*24*60*60))}'
